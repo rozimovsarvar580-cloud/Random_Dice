30a867ecf535efe820280fc2441531632486ccff
sarvar <rozimovsarvar580@gmail.com>
2026-02-09 18:05:27 +0300

    v1

---
+++ cube.js
@@ -0,0 +1,31 @@
+const cube = document.querySelector('.cube')
+let deg = [
+    '90',
+    '180',
+    '270',
+    '360',
+]
+let XY = [
+    'X',
+    'Y',
+]
+
+window.addEventListener('keydown',(e) =>{
+if(e.key === 'ArrowRight'){
+  cube.style.transform += 'rotateY(-90deg)'
+}
+if(e.key === 'ArrowLeft'){
+    cube.style.transform += 'rotateY(90deg)'
+}
+if(e.key === 'ArrowUp'){
+    cube.style.transform += 'rotateX(-90deg)'
+}
+if(e.key === 'ArrowDown'){
+    cube.style.transform += 'rotateX(90deg)'
+}
+})
+console.log(Math.trunc(Math.random() * 4))
+window.addEventListener('click', () =>{
+console.log(`rotate${XY[Math.trunc(Math.random()*2)]}(${deg[Math.trunc(Math.random()*4)]}deg)`)
+})
+    
