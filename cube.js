a7b564ad4755b26970ca1d12cafd1f1f5b806d89
sarvar <rozimovsarvar580@gmail.com>
2026-02-12 17:09:43 +0300

    v6

--- cube.js
+++ cube.js
@@ -1,39 +1,39 @@
-const dice = document.querySelector('.dice')
 const btn = document.querySelector('.btn')
+const dice = document.querySelector('.dice')
 const randomDice = () =>{
-    const random = Math.floor(Math.random()*10)
-    if(random >=1 && random <= 6){
-       Roll(random)
+    const random  = Math.floor(Math.random() *10)
+    if(random>= 1&& random<=6){
+        Roll(random)
     }else{
-    randomDice()
+        randomDice()
     }
 }
-const Roll = (random) =>{ 
+const Roll = (random) =>{
     dice.style.animation = 'rolling 4s'
- setTimeout(()=>{
-    switch(random){
-    case 1 :
-        dice.style.transform = 'rotateX(0deg) rotateY(0deg)'
-        break
-    case 6 :
-        dice.style.transform = 'rotateX(180deg) rotateY(0deg)'
-        break
-    case 2 :
-        dice.style.transform = 'rotateX(-90deg) rotateY(0deg)'
-        break
-    case 5 :
-        dice.style.transform = 'rotateX(90deg) rotateY(0deg)'
-        break
-    case 3 :
-        dice.style.transform = 'rotateX(0deg) rotateY(90deg)'
-        break
-    case 4 :
-        dice.style.transform = 'rotateX(0deg) rotateY(-90deg)'
-        break
-    default:
-        break
+    setTimeout(() =>{
+        switch(random){
+            case 1:
+                dice.style.transform = 'rotateX(0deg) rotateY(0deg)'
+                break
+            case 6 :
+                dice.style.transform = 'rotateX(180deg) rotateX(0deg)'
+                break
+            case 2:
+                dice.style.transform = 'rotateX(-90deg) rotateX(0deg)'
+                break
+            case 5 :
+                dice.style.transform = 'rotateX(90deg) rotateX(0deg)'
+                break
+            case 3:
+                dice.style.transform = 'rotateY(90deg) rotateX(0deg)'
+                break
+            case 4 :
+                dice.style.transform = 'rotateY(-90deg) rotateX(0deg)'
+                break
+        }
+        dice.style.animation = 'none'
+    },4050)
 }
-dice.style.animation = 'none'
- },4050)
-}
-btn.addEventListener('click', randomDice)
+btn.addEventListener('click',() =>{
+    randomDice()
+})
